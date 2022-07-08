f3b7fc020b7d54b61ff9483990fe3f869724cff8
SousaUA <hsousa@ua.pt>
2022-07-08 19:34:06 +0100

    Get Stops to CSV Format

--- scripts/datasource_to_csv.py
+++ scripts/datasource_to_csv.py
@@ -1,8 +1,16 @@
+from sys import argv
 def stops_to_csv(input, output):
     """
     Write stops to csv
     """
-    pass
+    with open(input, 'r') as f_in:
+        with open(output, 'w') as f_out:
+            f_out.write("name, lat, long\n")
+            for line in f_in:
+                clean_line = line.rstrip().replace("Stop", "").replace("(", "").replace(")", "").replace("\"", "").replace("Location", "")
+                f_out.write(clean_line + "\n")
+
+
 
 def routes_to_csv(input, output):
     """
@@ -15,7 +23,11 @@ def main():
     Main function
     """
     #TODO: Get input, output files and entity from command line
-    pass
 
+    input = argv[1]
+    output = argv[2]
+    entity = argv[3]
+
+    return stops_to_csv(input, output) if entity == "stops" else routes_to_csv(input, output)
 if __name__ == '__main__':
     main()
