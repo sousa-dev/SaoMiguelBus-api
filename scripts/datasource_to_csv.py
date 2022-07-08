689a720935d4e73e05112bbf582851dfff163ee8
SousaUA <hsousa@ua.pt>
2022-07-08 21:24:36 +0100

    Get info from datasource routes to array in python

--- scripts/datasource_to_csv.py
+++ scripts/datasource_to_csv.py
@@ -1,4 +1,5 @@
 from sys import argv
+
 def stops_to_csv(input, output):
     """
     Write stops to csv
@@ -16,18 +17,44 @@ def routes_to_csv(input, output):
     """
     Write routes to csv
     """
-    pass
+    routes = []
+    route = -1
+    type_of_day = None
+    details = []
+    with open(input, 'r') as f_in:
+        with open(output, 'w') as f_out:
+            f_out.write("stop, times, type_of_day\n")
+            for line in f_in:
+                if line.startswith("route"):
+                    type_of_day = None
+                    route = line.split()[-1].replace("\"", "")
+                    details = [route]
+                elif line.startswith("getStop"):
+                    clean_line = line.rstrip().replace("getStop", "").replace("(", "").replace(")", "").replace("\"", "").replace("to listOf", ",")
+                    temp_details = clean_line.strip().split(',')
+                    details.append([detail.strip() for detail in temp_details if detail != ""])
+                elif "TypeOfDay" in line:
+                    type_of_day = line.split(",")[1].replace("TypeOfDay.", "").strip()
+                    details.append(type_of_day)
+
+                    routes.append(details)
+
+                    details = [route]
+                    type_of_day = None
+    for route in routes:
+        #TODO: Process route to store it on csv file
+        print(route)
 
 def main():
     """
     Main function
     """
-    #TODO: Get input, output files and entity from command line
 
     input = argv[1]
     output = argv[2]
     entity = argv[3]
 
     return stops_to_csv(input, output) if entity == "stops" else routes_to_csv(input, output)
+
 if __name__ == '__main__':
     main()
