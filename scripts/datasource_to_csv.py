722e146c12ab5fd330ffa9528dce7f8e0e75d24b
SousaUA <hsousa@ua.pt>
2022-07-08 14:47:45 +0100

    init datasource to csv converter

---
+++ scripts/datasource_to_csv.py
@@ -0,0 +1,21 @@
+def stops_to_csv(input, output):
+    """
+    Write stops to csv
+    """
+    pass
+
+def routes_to_csv(input, output):
+    """
+    Write routes to csv
+    """
+    pass
+
+def main():
+    """
+    Main function
+    """
+    #TODO: Get input, output files and entity from command line
+    pass
+
+if __name__ == '__main__':
+    main()
