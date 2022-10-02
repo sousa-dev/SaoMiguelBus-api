c500f8344a000bcbdfcf2183b0f18d613e00b1d1
SousaUA <hsousa@ua.pt>
2022-10-02 15:50:10 +0100

    Add Stats to Admin Page

--- src/app/admin.py
+++ src/app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from app.models import Stop, Route
+from app.models import Stop, Route, Stat
 
 admin.site.register(Stop)
 admin.site.register(Route)
+admin.site.register(Stat)
