d70814be2c3471fbdc81e459cfe221bf24bbdcc0
SousaUA <hsousa@ua.pt>
2022-10-02 19:53:23 +0100

    get weekday from timestamp

--- scripts/extract_stats.py
+++ scripts/extract_stats.py
@@ -1,13 +1,35 @@
 import requests
+import pandas as pd
 
-response = requests.get('https://saomiguelbus-api.herokuapp.com/api/v1/stats').json()
+def get_weekday(timestamp):
+    year = timestamp.split("T")[0].split("-")[0]
+    month = timestamp.split("T")[0].split("-")[1]
+    day = timestamp.split("T")[0].split("-")[2]
+    ts = pd.Timestamp(int(year.lstrip("0")), int(month.lstrip("0")), int(day.lstrip("0")))
+    if ts.day_of_week == 0:
+        return "Monday"
+    elif ts.day_of_week == 1:
+        return "Tuesday"
+    elif ts.day_of_week == 2:
+        return "Wednesday"
+    elif ts.day_of_week == 3:
+        return "Thursday"
+    elif ts.day_of_week == 4:
+        return "Friday"
+    elif ts.day_of_week == 5:
+        return "Saturday"
+    elif ts.day_of_week == 6:
+        return "Sunday"
 
-stats = {}
+if __name__ == "__main__":
+    response = requests.get('https://saomiguelbus-api.herokuapp.com/api/v1/stats').json()
 
-for stat in response:
-    if stat['request'] in stats:
-        stats[stat['request']].append(stat)
-    else:
-        stats[stat['request']] = [stat]
+    stats = {}
 
-#print(f"Android Load Requests: {len(stats['android_load'])}\nGet Route Requests: {len(stats['get_route'])}")
+    for stat in response:
+        if stat['request'] in stats:
+            stats[stat['request']].append(stat)
+        else:
+            stats[stat['request']] = [stat]
+
+    #print(f"Android Load Requests: {len(stats['android_load'])}\nGet Route Requests: {len(stats['get_route'])}")
