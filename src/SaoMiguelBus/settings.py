b6a9b80f9177ab9054bddb51399c44f1dafa2b44
Henrique Sousa <56836057+sousaUA@users.noreply.github.com>
2023-12-10 22:12:50 +0000

    Update settings.py

--- src/SaoMiguelBus/settings.py
+++ src/SaoMiguelBus/settings.py
@@ -34,6 +34,27 @@ CORS_ALLOWED_ORIGINS = [
     'https://saomiguelbus.com',  # for production
 ]
 
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 # Application definition
 
 INSTALLED_APPS = [
