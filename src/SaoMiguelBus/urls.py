fc80bc7277ff13fbfe514f6135f0440f5f367b57
hc-sousa <henrique@sousadev.com>
2024-05-22 18:23:18 +0000

    Update urls.py

--- src/SaoMiguelBus/urls.py
+++ src/SaoMiguelBus/urls.py
@@ -40,5 +40,6 @@ urlpatterns = [
     path('api/v1/gmaps', views.get_gmaps_v1),
     path('api/v1/holidays', views.get_holidays_v1),
     path('api/v1/feriados', views.get_holidays_v1),
+    path('api/v1/datas', views.get_all_datas_v1),
     path('statistics', views.stats),
 ]
